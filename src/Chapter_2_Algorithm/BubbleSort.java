fabf6d3190bae5c2c714580ad38534d152c450bc
dasoopark <ektn0514@gmail.com>
2022-06-16 11:11:08 +0900

    Create BubbleSort.java

---
+++ src/Chapter_2_Algorithm/BubbleSort.java
@@ -0,0 +1,35 @@
+package Chapter_2_Algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BubbleSort {
+    public ArrayList<Integer> sort(ArrayList<Integer> dataList) {
+        for (int index = 0; index < dataList.size() - 1; index++) {
+            boolean swap = false;
+
+            for (int index2 = 0; index2 < dataList.size() - index - 1; index2++) {
+                if (dataList.get(index2) > dataList.get(index2 + 1)) {
+                    Collections.swap(dataList, index2, index2 + 1);
+                    swap = true; 
+                }
+            }
+
+            if (swap == false) {
+                break;
+            }
+        }
+        return dataList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> testData = new ArrayList<Integer>();
+
+        for (int i = 0; i < 100; i++) {
+            testData.add((int)(Math.random() * 100));
+        }
+        BubbleSort bSort = new BubbleSort();
+        System.out.println(bSort.sort(testData));
+    }
+
+}

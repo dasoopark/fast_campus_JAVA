9c162499c42f4bfceec77a47f19a16637b28d6a4
dasoopark <ektn0514@gmail.com>
2022-06-16 14:19:14 +0900

    선택정렬, 삽입정렬

---
+++ src/Chapter_2_Algorithm/InsertionSort.java
@@ -0,0 +1,10 @@
+package Chapter_2_Algorithm;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Chapter_2_Algorithm/SelectionSort.java
+++ src/Chapter_2_Algorithm/SelectionSort.java
@@ -1,15 +1,26 @@
 package Chapter_2_Algorithm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+//선택 정렬 
+/*
+ 	• 다음과 같은 순서를 반복하며 정렬하는 알고리즘
+		1. 주어진 데이터 중, 최소값을 찾음
+		2. 해당 최소값을 데이터 맨 앞에 위치한 값과 교체함
+		3. 맨 앞의 위치를 뺀 나머지 데이터를 동일한 방법으로 반복함
+ */
+
 public class SelectionSort {
     public ArrayList<Integer> sort(ArrayList<Integer> dataList) {
-        int lowest;
+        int lowest; //가장 작은 값 저장할 변수
         for (int stand = 0; stand < dataList.size() - 1; stand++) {
-            lowest = stand;
-            for (int index = stand + 1; index < dataList.size(); index++) {
+            lowest = stand; //첫 부분은 가장 작은 값 이므로 
+            for (int index = stand + 1; index < dataList.size(); index++) { //비교할 반복문
                 if (dataList.get(lowest) > dataList.get(index)) {
                     lowest = index;
                 }
-            }
+            } 
             Collections.swap(dataList, lowest, stand);
         }
         return dataList;

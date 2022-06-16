fb6a4624b5c05520ae01716c3415fc60e9483a6b
dasoopark <ektn0514@gmail.com>
2022-06-16 15:17:25 +0900

    삽입정렬

--- src/Chapter_2_Algorithm/BubbleSort.java
+++ src/Chapter_2_Algorithm/BubbleSort.java
@@ -3,6 +3,13 @@ package Chapter_2_Algorithm;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/*
+ 1. 삽입 정렬 (insertion sort) 란?
+	• 삽입 정렬은 두 번째 인덱스부터 시작
+	• 해당 인덱스(key 값) 앞에 있는 데이터(B)부터 비교해서 key 값이 더 작으면, B값을 뒤 인덱스로 복사
+이를 key 값이 더 큰 데이터를 만날때까지 반복, 그리고 큰 데이터를 만난 위치 바로 뒤에 key 값을 이동
+
+ */
 public class BubbleSort {
     public ArrayList<Integer> sort(ArrayList<Integer> dataList) {
         for (int index = 0; index < dataList.size() - 1; index++) {  

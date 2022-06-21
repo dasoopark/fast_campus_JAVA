3eb5e91931443bf7057ace234afa0e8ac97e254f
dasoopark <ektn0514@gmail.com>
2022-06-21 17:54:58 +0900

    Create Sequence_sum_1182.java

---
+++ src/YU_CHAP1_2_Brute_Force/Sequence_sum_1182.java
@@ -0,0 +1,110 @@
+package YU_CHAP1_2_Brute_Force;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ 부분수열의 합
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	256 MB	52131	24023	15471	44.342%
+
+문제
+N개의 정수로 이루어진 수열이 있을 때, 크기가 양수인 부분수열 중에서 그 수열의 원소를 다 더한 값이 S가 되는 경우의 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수의 개수를 나타내는 N과 정수 S가 주어진다. (1 ≤ N ≤ 20, |S| ≤ 1,000,000) 둘째 줄에 N개의 정수가 빈 칸을 사이에 두고 주어진다.
+ 주어지는 정수의 절댓값은 100,000을 넘지 않는다.
+
+ */
+public class Sequence_sum_1182 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        N = scan.nextInt();
+        S = scan.nextInt();
+        nums = new int[N + 1];
+        for (int i = 1; i <= N; i++) nums[i] = scan.nextInt();
+    }
+
+    static int N, S, ans;
+    static int[] nums;
+    // k번째 원소를 포함시킬 지 정하는 함수
+    // value:= k-1 번째 원소까지 골라진 원소들의 합
+    static void rec_func(int k, int value) {
+        if (k == N + 1) {  // 부분 수열을 하나 완성 시킨 상태
+            // value 가 S 랑 같은 지 확인하기
+            if (value == S) ans++;
+        } else {
+            // k 번째 원소를 포함시킬 지 결정하고 재귀호출해주기
+            // Include
+            rec_func(k + 1, value + nums[k]);
+            // Not Include
+            rec_func(k + 1, value);
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
+        rec_func(1, 0);
+        
+        // ans 가 정말 "진 부분집합(비어있는 부분집합)"만 다루는 지 확인하기
+        if (S == 0){//공집합 제거
+            ans--;
+        }
+        System.out.println(ans);
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

d744d248d367d1da7e35f6f391270b2f74b1529f
dasoopark <ektn0514@gmail.com>
2022-06-21 17:42:20 +0900

    Update Operator_Insert_14888.java

--- src/YU_CHAP1_2_Brute_Force/Operator_Insert_14888.java
+++ src/YU_CHAP1_2_Brute_Force/Operator_Insert_14888.java
@@ -1,14 +1,44 @@
 package YU_CHAP1_2_Brute_Force;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ 문제
+N개의 수로 이루어진 수열 A1, A2, ..., AN이 주어진다. 또, 수와 수 사이에 끼워넣을 수 있는 N-1개의 연산자가 주어진다. 연산자는 덧셈(+), 뺄셈(-), 곱셈(×), 나눗셈(÷)으로만 
+이루어져 있다.
+우리는 수와 수 사이에 연산자를 하나씩 넣어서, 수식을 하나 만들 수 있다. 이때, 주어진 수의 순서를 바꾸면 안 된다.
+예를 들어, 6개의 수로 이루어진 수열이 1, 2, 3, 4, 5, 6이고, 주어진 연산자가 덧셈(+) 2개, 뺄셈(-) 1개, 곱셈(×) 1개, 나눗셈(÷)
+ 1개인 경우에는 총 60가지의 식을 만들 수 있다. 예를 들어, 아래와 같은 식을 만들 수 있다.
+	• 1+2+3-4×5÷6
+	• 1÷2+3+4-5×6
+	• 1+2÷3×4-5+6
+	• 1÷2×3-4+5+6
+	
+식의 계산은 연산자 우선 순위를 무시하고 앞에서부터 진행해야 한다. 또, 나눗셈은 정수 나눗셈으로 몫만 취한다. 
+음수를 양수로 나눌 때는 C++14의 기준을 따른다. 즉, 양수로 바꾼 뒤 몫을 취하고, 그 몫을 음
+수로 바꾼 것과 같다. 이에 따라서, 위의 식 4개의 결과를 계산해보면 아래와 같다.
+
+	• 1+2+3-4×5÷6 = 1
+	• 1÷2+3+4-5×6 = 12
+	• 1+2÷3×4-5+6 = 5
+	• 1÷2×3-4+5+6 = 7
+	
+N개의 수와 N-1개의 연산자가 주어졌을 때, 만들 수 있는 식의 결과가 최대인 것과 최소인 것을 구하는 프로그램을 작성하시오.
+ */
 public class Operator_Insert_14888 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     static void input() {
-        N = scan.nextInt();
-        nums = new int[N + 1];
-        operators = new int[5];
-        order = new int[N + 1];
+        N = scan.nextInt(); 
+        nums = new int[N + 1]; //기록에 저장될 배열 
+        operators = new int[5]; //연산자 갯수 
         for (int i = 1; i <= N; i++) nums[i] = scan.nextInt();
         for (int i = 1; i <= 4; i++) operators[i] = scan.nextInt();
 
@@ -17,11 +47,19 @@ public class Operator_Insert_14888 {
     }
 
     static int N, max, min;
-    static int[] nums, operators, order;
+    static int[] nums, operators;
 
     // 피연산자 2개와 연산자가 주어졌을 때 계산해주는 함수
     static int calculator(int operand1, int operator, int operand2){
-        // TODO
+        // value, order[i], num[i+1]
+        if (operator == 1) // +
+            return operand1 + operand2;
+        else if (operator == 2) // -
+            return operand1 - operand2;
+        else if (operator == 3) // *
+            return operand1 * operand2;
+        else // /
+            return operand1 / operand2;
     }
 
 
@@ -29,10 +67,17 @@ public class Operator_Insert_14888 {
     static void rec_func(int k, int value) {
         if (k == N) {
             // 완성된 식에 맞게 계산을 해서 정답에 갱신하는 작업
-            // TODO
+            max = Math.max(max, value); //지금까지의 최대값을 계산해주고 
+            min = Math.min(min, value); //지금까지의 최소값을 계산해준다
         } else {
             // k 번째 연산자는 무엇을 선택할 것인가?
-            // TODO
+            for (int cand = 1; cand <= 4; cand++){
+                if (operators[cand] >= 1){ //1이상이면 사용할 수 있는 상태
+                    operators[cand]--; //남은 횟수를 감소 
+                    rec_func(k + 1, calculator(value, cand, nums[k + 1]));
+                    operators[cand]++;
+                }
+            }
         }
     }
 

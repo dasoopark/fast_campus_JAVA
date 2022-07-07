379e7375cbd56db5357e6d8969b3fdb6174c83eb
dasoopark <ektn0514@gmail.com>
2022-07-07 21:04:30 +0900

    Update SumSet_1806.java

--- src/YU_CHAP4_TwoPointer/SumSet_1806.java
+++ src/YU_CHAP4_TwoPointer/SumSet_1806.java
@@ -1,5 +1,13 @@
 package YU_CHAP4_TwoPointer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
  부분합 다국어
 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
@@ -25,10 +33,90 @@ package YU_CHAP4_TwoPointer;
 
  */
 public class SumSet_1806 {
+    static StringBuilder sb = new StringBuilder();
+    static FastReader scan = new FastReader();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    static int n, S;
+    static int[] a;
 
-	}
+    static void input() {
+        n = scan.nextInt();
+        S = scan.nextInt();
+        a = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = scan.nextInt();
+        }
+    }
 
+    static void pro() {
+        int R = 0, sum = 0, ans = n + 1;
+        for (int L = 1; L <= n; L++) {
+            // L - 1 을 구간에서 제외하기
+            sum -= a[L - 1];
+            
+            // R 을 옮길 수 있을 때 까지 옮기기
+            while (R + 1 <= n && sum < S)
+                sum += a[++R];
+            
+            // [L ... R] 의 합, 즉 sum이 조건을 만족하면 정답 갱신하기
+            if (sum >= S)
+                ans = Math.min(ans, R - L + 1);
+        }
+
+        // ans 값을 보고 불가능 판단하기
+        if (ans == n + 1)
+            ans = 0;
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }

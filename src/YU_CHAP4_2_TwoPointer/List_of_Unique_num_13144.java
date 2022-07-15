6fe4c6dad8b4e0760943438bb605bc851c9e066e
dasoopark <ektn0514@gmail.com>
2022-07-15 22:14:09 +0900

    Update List_of_Unique_num_13144.java

--- src/YU_CHAP4_2_TwoPointer/List_of_Unique_num_13144.java
+++ src/YU_CHAP4_2_TwoPointer/List_of_Unique_num_13144.java
@@ -28,17 +28,17 @@ public class List_of_Unique_num_13144 {
         long ans = 0;
 
         for (int L=1, R=0; L<=N; L++){  // L 마다 R 을 최대한 옮겨 줄 계획이다.
-            // R 을 옮길 수 있는 만큼 옮긴다.
+            // R 을 옮길 수 있는 만큼 옮긴다.d 
             while (R + 1 <= N && cnt[A[R+1]] == 0){
                 R++;
-                cnt[A[R]]++; //해당 배열 인덱스 값 증가 
+                cnt[A[R]]++; //해당 배열 인덱스 값 증가  
             }
             
             // 정답을 갱신한다
-            ans += R - L + 1;
+            ans += R - L + 1; 
 
             // L 을 옮겨주면서 A[L] 의 개수를 감소시킨다.
-            cnt[A[L]]--;
+            cnt[A[L]]--; 
         }
 
         System.out.println(ans);

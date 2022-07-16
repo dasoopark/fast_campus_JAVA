90826ca2a3db473ef65a68b6abc32b43f6ad79ac
dasoopark <ektn0514@gmail.com>
2022-07-16 21:55:40 +0900

    Update two_potion_towpointer_2470.java

--- src/YU_CHAP4_TwoPointer/two_potion_towpointer_2470.java
+++ src/YU_CHAP4_TwoPointer/two_potion_towpointer_2470.java
@@ -27,15 +27,14 @@ public class two_potion_towpointer_2470 {
     static void pro() {
         // 최소, 최대 원소를 빠르게 찾기 위해서 정렬을 미리 해주자.
         Arrays.sort(A, 1, N + 1);
-
-        int best_sum = Integer.MAX_VALUE;
+        int best_sum = Integer.MAX_VALUE;  
         int v1 = 0, v2 = 0, L = 1, R = N;
 
         while (L < R){  // L == R 인 상황이면 용액이 한 개 뿐인 것이므로, L < R 일 때까지만 반복한다.
             if (best_sum > Math.abs(A[L] + A[R])) {
                 best_sum = Math.abs(A[L] + A[R]);
-                v1 = A[L];
-                v2 = A[R];
+                v1 = A[L]; 
+                v2 = A[R]; 
             }
             if (A[L] + A[R] > 0) R--;
             else L++;

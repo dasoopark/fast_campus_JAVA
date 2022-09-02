27795310cacaced8676d6fd2d75bcef2e6821c93
dasoopark <ektn0514@gmail.com>
2022-09-02 17:29:37 +0900

    Update lineUp_2252.java

--- src/YU_CHAP_7_WISANG_SORT/lineUp_2252.java
+++ src/YU_CHAP_7_WISANG_SORT/lineUp_2252.java
@@ -30,7 +30,7 @@ public class lineUp_2252 {
             int x = scan.nextInt(), y = scan.nextInt();
             adj[x].add(y);
             // indegree 계산하기
-            indeg[y]++; //인접 개수 증가
+            indeg[y]++; //인접 개수 증가   
         }
     }
 
